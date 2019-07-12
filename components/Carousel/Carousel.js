8c9bdb2968ed840a363a63addb0a3ea2781e24d4
Eli Sacks <bastlifa@gmail.com>
2019-07-12 12:03:56 -0400

    beginnings of carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,37 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function CarouselMaker()
+{
+  let carouselDiv = document.createElement("div")
+  let leftDiv = document.createElement("div")
+  let rightDiv = document.createElement("div")
+  let img0 = document.createElement("img")
+  let img1 = document.createElement("img")
+  let img2 = document.createElement("img")
+  let img3 = document.createElement("img")
+
+  carouselDiv.classList.add("carousel")
+  leftDiv.classList.add("left-button")
+  leftDiv.textContent = '<'
+  rightDiv.classList.add("right-button")
+  rightDiv.textContent = '>'
+
+  img0.src= "./assets/carousel/mountains.jpeg"
+  img1.src= "./assets/carousel/computer.jpeg"
+  img2.src= "./assets/carousel/trees.jpeg"
+  img3.src= "./assets/carousel/turntable.jpeg"
+
+  carouselDiv.appendChild(leftDiv)
+  carouselDiv.appendChild(img0)
+  carouselDiv.appendChild(img1)
+  carouselDiv.appendChild(img2)
+  carouselDiv.appendChild(img3)
+  carouselDiv.appendChild(rightDiv)
+
+  return carouselDiv;
+}
+
+document.querySelector(".carousel-container").appendChild(CarouselMaker())
+
